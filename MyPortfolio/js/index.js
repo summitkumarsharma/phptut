36474ba59fec66cb536b0364476e1e9974324155
summitkumarsharma <65065936+summitkumarsharma@users.noreply.github.com>
2020-07-11 02:18:11 +0530

    Initial commit

---
+++ MyPortfolio/js/index.js
@@ -0,0 +1,9 @@
+// var hovering = function(){
+//     $("nav").show("slide", { direction: "right" }, 1000);
+//  };
+ 
+//  var leaving = function(){
+//     $("nav").hide("slide", { direction: "left" }, 1000);
+//  };
+ 
+//  $("#nav-container").hover(hovering, leaving);
